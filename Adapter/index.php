38a38dc83d1b7cbbd874f6fb3495225e4e9df2e6
maxim.b <maxim.b@intensa.ru>
2024-06-12 09:43:22 +0300

    Comments for Adapter

--- Adapter/index.php
+++ Adapter/index.php
@@ -1,16 +1,21 @@
 <?php
 
+
 interface Duck
 {
+    // Утки крякают
     public function quack();
 
+    // и летают
     public function fly();
 }
 
 interface Turkey
 {
+    // Индюшки не крякают (у них нет метода quack())
     public function gobble();
 
+    // но могут летать, хотя и недалеко
     public function fly();
 }
 
@@ -27,12 +32,15 @@ class MallardDuck implements Duck
     }
 }
 
+// Конкретная реализация обобщенного класса Turkey; как и класс MallardDuck, она просто выводит описания своих действий
 class WildTurkey implements Turkey
 {
+
     public function gobble(): void
     {
         echo "WildTurkey gobble\n";
     }
+
 
     public function fly(): void
     {
@@ -40,21 +48,27 @@ class WildTurkey implements Turkey
     }
 }
 
+// Реализация адаптера
+
+// Прежде всего необходимо реализовать интерфейс того типа, на который рассчитан ваш клиент
 class TurkeyAdapter implements Duck
 {
 
     private Turkey $turkey;
 
+    // Затем следует получить ссылку на адаптируемый объект; обычно это делается в конструкторе
     public function __construct(Turkey $turkey)
     {
         $this->turkey = $turkey;
     }
 
+    // Адаптер должен реализовать все методы интерфейса. Преобразование quack() между классами выполняется просто - реализация вызывает gobble()
     public function quack(): void
     {
         $this->turkey->gobble();
     }
 
+    // Хотя метод fly() входит в оба интерфейса, индюшка не умеет летать на дальние расстояния. Чтобы устанновить соответствие между этими методами, мы вызываем метод fly() класса Turkey три раза
     public function fly(): void
     {
         for ($i = 0; $i < 3; $i++) {
@@ -63,7 +77,7 @@ class TurkeyAdapter implements Duck
     }
 }
 
-// testing
+// Тестирование адаптера
 $mallardDuct = new MallardDuck();
 
 $wildTurkey = new TurkeyAdapter(new WildTurkey());
@@ -77,3 +91,13 @@ function testDuck(Duck $duck): void
     $duck->quack();
     $duck->fly();
 }
+
+// Скрипт выводит
+/*
+MallardDuck quack
+MallardDuck fly
+WildTurkey gobble
+WildTurkey fly
+WildTurkey fly
+WildTurkey fly
+*/
